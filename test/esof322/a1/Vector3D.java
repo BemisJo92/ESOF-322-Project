f4e148d4458efdbd736da81a1e566d5d9b4361dc
BemisJo92 <bidinc.33@gmail.com>
2017-10-01 17:08:07 -0600

    Added negate method

---
+++ test/esof322/a1/Vector3D.java
@@ -0,0 +1,20 @@
+package pa1;
+
+public class Vector3D {
+	
+	double x; 
+	double y;
+	double z;
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public void negate()
+	{
+		x *= -1;
+		y *= -1;
+		z *= -1;
+	}
+}
