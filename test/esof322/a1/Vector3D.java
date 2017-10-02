0a22152b4ff49488033ef5eb1e7266588e9c18db
Logan Davis <logand222@gmail.com>
2017-10-01 18:23:59 -0600

    Added the methods of ADD and SUBTRACT

--- test/esof322/a1/Vector3D.java
+++ test/esof322/a1/Vector3D.java
@@ -20,4 +20,20 @@ public class Vector3D {
 		this.z = z; 		
 	}
 	
+	public Vector3D add(Vector3D v) 
+	{
+		this.x += v.x;
+		this.y += v.y;
+		this.z += v.z; 
+		return this; 
+	}
+	
+	public Vector3D subtract(Vector3D v)
+	{
+		this.x -= v.x;
+		this.y -= v.y;
+		this.z -= v.z;
+		return this;
+	}
+	
 }
