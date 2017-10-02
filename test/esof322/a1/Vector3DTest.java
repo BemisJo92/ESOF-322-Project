36c54c71e9c2dc90c2c322155fbeae458c99d2d8
Ian Hecker <ianhecker@yahoo.com>
2017-10-01 19:57:08 -0600

    Adding Joel's and Ian's files to our Ian_Joel branch

--- test/esof322/a1/Vector3DTest.java
+++ test/esof322/a1/Vector3DTest.java
@@ -1,3 +1,4 @@
+//
 package pa1;
 
 import static org.junit.Assert.*;
@@ -6,50 +7,56 @@ import org.junit.Test;
 
 public class Vector3DTest {
 
+	//Global Variables
+	double x = 2.0;
+	double y = 3.0;
+	double z = 4.0;
+	double scalor = 2.0;	
+		
 	@Test
-	public void testAdd()
-	{
-		Vector3D v1 = new Vector3D(1,2,3);
-		Vector3D v2 = new Vector3D(1,2,3);
+	public void test() {		
+		//Instance of Vector3D
+		Vector3D v1 = new Vector3D(x, y, z);
 		
-		Vector3D vectorResult = v1.add(v2);
+		//Method Calling
+		testEquals(x, v1.x);
+		testEquals(y, v1.y);
+		testEquals(z, v1.z);
 		
-		assertEquals(2, vectorResult.x, Double.NaN);
-		assertEquals(4, vectorResult.y, Double.NaN);
-		assertEquals(6, vectorResult.z, Double.NaN);
+		testScale(scalor, v1);
 		
-	}
-
-	@Test
-	public void testSubtract() 
-	{
-		Vector3D v1 = new Vector3D(1,2,3);
-		Vector3D v2 = new Vector3D(1,2,3);
-		
-		Vector3D vectorResult = v1.subtract(v2);
-		
-		assertEquals(0, vectorResult.x, Double.NaN);
-		assertEquals(0, vectorResult.y, Double.NaN);
-		assertEquals(0, vectorResult.z, Double.NaN);
+		testMagnitude(v1);
 	}
 	
-	@Test
-	public void testNegate() 
+	//Compares two doubles to check equality
+	public void testEquals(double expected, double testing)
 	{
-		Vector3D vectorResult = new Vector3D(1, 2, 3);
-		vectorResult.negate();
-		double result = vectorResult.x + vectorResult.y + vectorResult.z;
-		assertEquals("Result", -6, result, Double.NaN);
-		assertEquals(-6, result, Double.NaN);
+		assertEquals(expected, testing, Double.NaN);
 	}
-
-	@Test
-	public void testDot()
+	
+	//Tests the instance's multiples from Scale() method with expected 
+	//math for multiplying
+	public void testScale(double scalor, Vector3D instance)
+	{		
+		double scalorX, scalorY, scalorZ;
+		
+		scalorX = x * scalor;
+		scalorY = y * scalor;
+		scalorZ = z * scalor;		
+		
+		Vector3D v2 = instance.scale(scalor);
+		//Compare instance's vars with expected results
+		testEquals(scalorX, v2.x);
+		testEquals(scalorY, v2.y);
+		testEquals(scalorZ, v2.z);				
+	}
+	
+	public void testMagnitude(Vector3D instance)
 	{
-		Vector3D v1 = new Vector3D(1.0, 2.0, 3.0);
-		Vector3D v2 = new Vector3D(2.0, 3.0, 4.0);
-		double result = v1.dot(v2);
-		assertEquals("Result", 20.0, result, Double.NaN);
-		assertEquals(20.0, result, Double.NaN);
+		double magnitude;
+		magnitude = Math.sqrt((x*x + y*y + z*z));
+		
+		assertEquals(magnitude, instance.magnitude(), Double.NaN);
 	}
+	
 }
