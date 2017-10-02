e17d9d46d4828135ec08c309dfb1b8360cfda90e
BemisJo92 <bidinc.33@gmail.com>
2017-10-01 18:28:19 -0600

    Updating to master branch

--- test/esof322/a1/Vector3D.java
+++ test/esof322/a1/Vector3D.java
@@ -1,5 +1,32 @@
 package pa1;
 
 public class Vector3D {
-
+	
+	double x; 
+	double y;
+	double z;
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public Vector3D(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public void negate()
+	{
+		x *= -1;
+		y *= -1;
+		z *= -1;
+	}
+	
+	double dot(Vector3D v)
+	{
+		return (this.x * v.x) + (this.y * v.y) + (this.z * v.z);
+	}
 }
