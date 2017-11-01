f2e44aba77b639e201878968c2fdb78c1e5233d6
Logan Davis <logand222@gmail.com>
2017-10-31 22:04:31 -0600

    Test GUI in game class

--- test/esof322/MonopolyGame/src/monopoly/Game.java
+++ test/esof322/MonopolyGame/src/monopoly/Game.java
@@ -8,4 +8,40 @@ import javax.swing.JFrame;
 
 public class Game {
 
+        public static void addComponentsToPane(Container pane) {
+            
+        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+ 
+        addButton("Button 1", pane);
+        
+    }
+ 
+    private static void addButton(String text, Container container) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(button);
+    }
+ 
+    
+    private static void showGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Test GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+ 
+        //Display the window.
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                showGUI();
+            }
+        });
+    }
 }
