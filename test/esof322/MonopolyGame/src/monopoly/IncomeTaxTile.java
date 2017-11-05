005be6a4cac8793bb891e2bd232238314e1b6c8f
Hedwigg <Hedwigg01@gmail.com>
2017-11-05 15:05:19 -0700

    remove money for income & luxury tax fixed error throwing for free parking tile

--- test/esof322/MonopolyGame/src/monopoly/IncomeTaxTile.java
+++ test/esof322/MonopolyGame/src/monopoly/IncomeTaxTile.java
@@ -3,7 +3,7 @@ package monopoly;
 
 
 public class IncomeTaxTile extends Tile{
-
+    private final int incomeTaxAmount = 200;
     public IncomeTaxTile(String name) {
         super(name);
     }
@@ -11,7 +11,8 @@ public class IncomeTaxTile extends Tile{
     @Override
     public void doAction(Player player, Board board) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        System.out.println("Landed on Income Tax tile");               
+        System.out.println("Landed on Income Tax tile");       
+        player.removeMoney(incomeTaxAmount);
     }
     
 }
