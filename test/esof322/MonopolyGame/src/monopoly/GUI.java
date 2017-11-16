0589be623e1d0af17f4d0e93f3ce27c3d51ac5e3
Hedwigg <Hedwigg01@gmail.com>
2017-11-15 22:37:51 -0700

    Fixed GUI from creating a new Jframe window each player turn. -Joel

--- test/esof322/MonopolyGame/src/monopoly/GUI.java
+++ test/esof322/MonopolyGame/src/monopoly/GUI.java
@@ -8,11 +8,12 @@ import java.io.File;
 import java.io.IOException;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
-public class GUI{
+public class GUI extends JPanel{
     
-    JFrame window;  //origional window
+    JFrame window;  //orig window
     static int[] p1Coords = new int[2];
     static int[] p2Coords = new int[2];
     static int[] p3Coords = new int[2];
@@ -20,6 +21,7 @@ public class GUI{
     
     public void setup()
     {
+        
         //set origional player coords
         p1Coords[0] =490;
         p1Coords[1] = 490;
@@ -84,14 +86,13 @@ public class GUI{
                 break;
         }
         
-        //update GUI
-        //window.setVisible(false);
-        JFrame newWindow = new JFrame("Monopoly");
-        newWindow.setBounds(0,0,600,600);
-        newWindow.getContentPane().add(new Canvas());
-        newWindow.setVisible(true);
-        newWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          window.repaint(); //update jframe
 
+    }
+    
+    public void display(String s)     //call instead of println's in rest of program to output text to gui.
+    {
+        
     }
     
 }
@@ -100,18 +101,28 @@ class Canvas extends JComponent{
     private final int iconWidth = 20;
     private final int iconHeight = 20;
     private Image backgroundImage;
-    private int p1X = GUI.getCoords(0)[0];
-    private int p1Y = GUI.getCoords(0)[1];
-    private int p2X = GUI.getCoords(1)[0];
-    private int p2Y = GUI.getCoords(1)[1];
-    private int p3X = GUI.getCoords(2)[0];
-    private int p3Y = GUI.getCoords(2)[1];
-    private int p4X = GUI.getCoords(3)[0];
-    private int p4Y = GUI.getCoords(3)[1];
     
+    private int p1X;
+    private int p1Y;
+    private int p2X;
+    private int p2Y;
+    private int p3X;
+    private int p3Y;
+    private int p4X;
+    private int p4Y;
     
-    public void paint(Graphics g)
+    public void paintComponent(Graphics g)
     {
+        int p1X = GUI.getCoords(0)[0];
+        int p1Y = GUI.getCoords(0)[1];
+        int p2X = GUI.getCoords(1)[0];
+        int p2Y = GUI.getCoords(1)[1];
+        int p3X = GUI.getCoords(2)[0];
+        int p3Y = GUI.getCoords(2)[1];
+        int p4X = GUI.getCoords(3)[0];
+        int p4Y = GUI.getCoords(3)[1];
+        
+        super.paintComponent(g);
         try
         {   
             backgroundImage = javax.imageio.ImageIO.read(new File("monopoly board.jpg"));
@@ -120,6 +131,7 @@ class Canvas extends JComponent{
              throw new RuntimeException(e);
         }
         g.drawImage(backgroundImage, WIDTH, WIDTH, this);   //background image
+       
         g.setColor(Color.blue);
         g.fillOval(p1X, p1Y, iconWidth, iconHeight); //p1
         g.setColor(Color.red);
