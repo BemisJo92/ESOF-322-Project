55b9e1b6e3add78f94d9cb1a0d845e994f96f59b
Hedwigg <Hedwigg01@gmail.com>
2017-11-26 19:45:39 -0700

    added test cases for GUI.movePlayer() to cover all of the players in the switch statement

--- test/esof322/MonopolyGame/test/monopoly/GUITest.java
+++ test/esof322/MonopolyGame/test/monopoly/GUITest.java
@@ -36,19 +36,47 @@ public class GUITest {
     @After
     public void tearDown() {
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
     
     @Test
-    public void testMovePlayer()
+    public void testMovePlayer0()
     {
         GUI g = new GUI();
         g.setup();
         Player p = new Player(0, "testName");
+        PropertyTile t = new PropertyTile("Indiana Ave",160,40, 220, 18,90,250,700,875,1050,150,150,110);
+        g.movePlayer(p.getIdNum(), t.getX(), t.getY());
+        int expected = 40;
+        int actual = t.getX();
+    }
+    
+    @Test
+    public void testMovePlayer1()
+    {
+        GUI g = new GUI();
+        g.setup();
+        Player p = new Player(1, "testName1");
+        PropertyTile t = new PropertyTile("Indiana Ave",160,40, 220, 18,90,250,700,875,1050,150,150,110);
+        g.movePlayer(p.getIdNum(), t.getX(), t.getY());
+        int expected = 40;
+        int actual = t.getX();
+    }
+    @Test
+    public void testMovePlayer2()
+    {
+        GUI g = new GUI();
+        g.setup();
+        Player p = new Player(2, "testName2");
+        PropertyTile t = new PropertyTile("Indiana Ave",160,40, 220, 18,90,250,700,875,1050,150,150,110);
+        g.movePlayer(p.getIdNum(), t.getX(), t.getY());
+        int expected = 40;
+        int actual = t.getX();
+    }
+    @Test
+    public void testMovePlayer3()
+    {
+        GUI g = new GUI();
+        g.setup();
+        Player p = new Player(3, "testName3");
         PropertyTile t = new PropertyTile("Indiana Ave",160,40, 220, 18,90,250,700,875,1050,150,150,110);
         g.movePlayer(p.getIdNum(), t.getX(), t.getY());
         int expected = 40;
